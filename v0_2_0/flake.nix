282b40e6ff363a0641cf507b50b43be66d7e4360
hugosenari <hugosenari@gmail.com>
2022-03-22 00:25:30 -0300

    chore: re-index ../nimpkgs/c/camelize/v0_2_0

--- v0_2_0/flake.nix
+++ v0_2_0/flake.nix
@@ -9,7 +9,7 @@
   
   inputs.src-camelize-v0_2_0.flake = false;
   inputs.src-camelize-v0_2_0.owner = "kixixixixi";
-  inputs.src-camelize-v0_2_0.ref   = "refs/tags/v0.2.0";
+  inputs.src-camelize-v0_2_0.ref   = "v0_2_0";
   inputs.src-camelize-v0_2_0.repo  = "camelize";
   inputs.src-camelize-v0_2_0.type  = "github";
   
