64d274c2eec03d43a7473ee4ff5decee126e045e
hugosenari <hugosenari@gmail.com>
2022-05-09 20:35:52 -0300

    chore: re index camelize

--- flake.nix
+++ flake.nix
@@ -7,38 +7,6 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs."camelize-master".dir   = "master";
-  inputs."camelize-master".owner = "nim-nix-pkgs";
-  inputs."camelize-master".ref   = "master";
-  inputs."camelize-master".repo  = "camelize";
-  inputs."camelize-master".type  = "github";
-  inputs."camelize-master".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."camelize-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
-  inputs."camelize-v0_1_0".dir   = "v0_1_0";
-  inputs."camelize-v0_1_0".owner = "nim-nix-pkgs";
-  inputs."camelize-v0_1_0".ref   = "master";
-  inputs."camelize-v0_1_0".repo  = "camelize";
-  inputs."camelize-v0_1_0".type  = "github";
-  inputs."camelize-v0_1_0".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."camelize-v0_1_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
-  inputs."camelize-v0_2_0".dir   = "v0_2_0";
-  inputs."camelize-v0_2_0".owner = "nim-nix-pkgs";
-  inputs."camelize-v0_2_0".ref   = "master";
-  inputs."camelize-v0_2_0".repo  = "camelize";
-  inputs."camelize-v0_2_0".type  = "github";
-  inputs."camelize-v0_2_0".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."camelize-v0_2_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
-  inputs."camelize-v0_2_1".dir   = "v0_2_1";
-  inputs."camelize-v0_2_1".owner = "nim-nix-pkgs";
-  inputs."camelize-v0_2_1".ref   = "master";
-  inputs."camelize-v0_2_1".repo  = "camelize";
-  inputs."camelize-v0_2_1".type  = "github";
-  inputs."camelize-v0_2_1".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."camelize-v0_2_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
